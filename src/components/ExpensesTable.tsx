7b2f35728bf307c181819c4a4ea24ca8cb0a4286
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-11 19:25:40 -0500

    add editable input to cell amount

--- src/components/ExpensesTable.tsx
+++ src/components/ExpensesTable.tsx
@@ -1,13 +1,44 @@
 // import expenses from '../mocks/expenses.json'
+// import type { expenseType } from '../types/expense'
 import styles from './ExpensesTable.module.css'
 import { DeleteIcon } from './icons/DeleteIcon'
-// import type { expenseType } from '../types/expense'
 import { useExpenses } from '../hooks/useExpenses'
 import { AddExpenseForm } from './AddExpenseForm'
+import { useState } from 'react'
+import React from 'react'
 
 export const ExpensesTable = () => {
   // const results: expenseType[] = expenses.results
-  const { expenses, removeExpense } = useExpenses()
+  const { expenses, setExpenses, removeExpense } = useExpenses()
+
+  interface cellType {
+    rowId: string | null
+    field: string | null
+  }
+
+  const [editingCell, setEditingCell] = useState<cellType>({
+    rowId: null,
+    field: null
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    rowId: string,
+    field: string
+  ) => {
+    const updatedExpenses = expenses.map((row) =>
+      row.id === rowId ? { ...row, [field]: e.target.value } : row
+    )
+    setExpenses(updatedExpenses)
+  }
+
+  const handleDoubleClick = (rowId: string, field: string) => {
+    setEditingCell({ rowId, field })
+  }
+
+  const handleBlur = () => {
+    setEditingCell({ rowId: null, field: null })
+  }
 
   return (
     <div className={styles.container}>
@@ -36,11 +67,38 @@ export const ExpensesTable = () => {
                 expenses.map((expense, i) => (
                   <tr key={expense.id}>
                     <td className={styles.tData}>{i + 1}</td>
-                    <td className={styles.tData}>
-                      <span className={styles.sign}>$</span>
-                      {expense.amount}
+                    <td
+                      className={styles.tData}
+                      onDoubleClick={() => {
+                        handleDoubleClick(expense.id, 'amount')
+                      }}
+                    >
+                      {editingCell.rowId === expense.id &&
+                      editingCell.field === 'amount' ? (
+                        <input
+                          type='text'
+                          value={expense['amount']} // Valor actual de la celda
+                          autoFocus // Foco automático al entrar al modo edición
+                          onChange={(e) =>
+                            handleChange(e, expense.id, 'amount')
+                          } // Actualiza el estado al escribir
+                          onBlur={handleBlur} // Sale del modo edición al perder foco
+                        />
+                      ) : (
+                        <>
+                          <span className={styles.sign}>$</span>
+                          {expense.amount}
+                        </>
+                      )}
                     </td>
-                    <td className={styles.tData}>{expense.category}</td>
+                    <td
+                      className={styles.tData}
+                      onClick={() => {
+                        handleDoubleClick(expense.id, 'category')
+                      }}
+                    >
+                      {expense.category}
+                    </td>
                     <td className={styles.tData}>
                       <div className={styles.flexSE}>
                         <button

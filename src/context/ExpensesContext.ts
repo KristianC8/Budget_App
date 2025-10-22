393d22608e97346392e8a1fff3836613d0d4a58d
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-22 18:41:19 -0500

    refactor: added context with database sync

---
+++ src/context/ExpensesContext.ts
@@ -0,0 +1,17 @@
+import { createContext } from 'react'
+import type { Expense, Errors } from '../types/dataBase'
+
+export interface ExpensesContextType {
+  expenses: Expense[]
+  loading: boolean
+  error: Errors
+  addExpense: (expense: Omit<Expense, 'id'>) => Promise<number | undefined>
+  updateExpense: (
+    id: number,
+    field: string,
+    update: number | string
+  ) => Promise<void>
+  deleteExpense: (id: number) => Promise<void>
+}
+
+export const ExpensesContext = createContext<ExpensesContextType | null>(null)

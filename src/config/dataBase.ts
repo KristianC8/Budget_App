a37643ba9651d459222c44dc9837fdda1b36644d
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-22 19:54:24 -0500

    feature(db): Added config for create databese

---
+++ src/config/dataBase.ts
@@ -0,0 +1,23 @@
+import type { StoreConfig } from '../types/dataBase'
+
+export const DB_NAME = 'BudgetAppDB'
+export const DB_VERSION = 1
+
+export const STORES: StoreConfig[] = [
+  {
+    name: 'expenses',
+    keyPath: 'id',
+    autoIncrement: true,
+    indexes: [
+      { name: 'createdAt', keyPath: 'createdAt', options: { unique: false } }
+    ]
+  },
+  {
+    name: 'categories',
+    keyPath: 'id',
+    autoIncrement: true,
+    indexes: [
+      { name: 'createdAt', keyPath: 'createdAt', options: { unique: false } }
+    ]
+  }
+]

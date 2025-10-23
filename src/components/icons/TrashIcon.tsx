58388fe573c96fcdbe04f56e82f444f518b9347a
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-23 18:39:25 -0500

    refactor: changed svg icon trash

--- src/components/icons/TrashIcon.tsx
+++ src/components/icons/TrashIcon.tsx
@@ -5,16 +5,12 @@ export const TrashIcon = () => {
       width='24'
       height='24'
       viewBox='0 0 24 24'
-      style={{ color: '#181818' }}
     >
       <path
-        fill='none'
-        stroke='currentColor'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3'
+        fill='currentColor'
+        d='M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z'
       />
+      <path fill='currentColor' d='M9 10h2v8H9zm4 0h2v8h-2z' />
     </svg>
   )
 }

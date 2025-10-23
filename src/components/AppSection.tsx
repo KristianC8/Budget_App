f6a12cd370bbab4cf8b8e204f8ba69cf902b1016
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-22 19:57:05 -0500

    refactor: Added new component for test in the section

--- src/components/AppSection.tsx
+++ src/components/AppSection.tsx
@@ -1,10 +1,13 @@
 import styles from './AppSection.module.css'
+import { ExpensesList } from './ExpensesList'
 import { ExpensesTable } from './ExpensesTable'
 
 export const AppSection = () => {
   return (
     <section className={styles.section}>
-      <div className={styles.left}></div>
+      <div className={styles.left}>
+        <ExpensesList />
+      </div>
 
       <div className={styles.right}>
         <ExpensesTable />

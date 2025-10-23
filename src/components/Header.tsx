6499926c764bb2638765a41db776833eb3b5b175
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-23 13:04:14 -0500

    feature: Added navigation to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import styles from './Header.module.css'
+import { Link } from 'wouter'
+import { SettingsIcon } from './icons/SettingsIcon'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Budget App</h1>
+      <h1 className={styles.title}>Presupuesto</h1>
+      <nav>
+        <Link href='/config' className={styles.link}>
+          <SettingsIcon />
+        </Link>
+      </nav>
     </header>
   )
 }

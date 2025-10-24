bab445204ad46336d442a78cc5e0f3467ad99630
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-24 11:49:05 -0500

    styles: add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import styles from './Header.module.css'
 import { Link } from 'wouter'
 import { SettingsIcon } from './icons/SettingsIcon'
+import { FinanceLogo } from './icons/FinanceLogo'
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Presupuesto</h1>
+      <div className={styles.flexH}>
+        <FinanceLogo />
+        <h1 className={styles.title}>Presupuesto</h1>
+      </div>
       <nav>
         <Link href='/config' className={styles.link}>
           <SettingsIcon />

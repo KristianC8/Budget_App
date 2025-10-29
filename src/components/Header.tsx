a2c4568857b58176d857235c24bad2059e525c5f
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-10-29 08:39:26 -0500

    feature: added title for config page in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,24 @@ import styles from './Header.module.css'
 import { Link } from 'wouter'
 import { SettingsIcon } from './icons/SettingsIcon'
 import { FinanceLogo } from './icons/FinanceLogo'
+import { useLocation } from 'wouter'
 
 export const Header = () => {
+  const [location] = useLocation()
   return (
     <header className={styles.header}>
       <Link href='/' className={styles.flexH}>
         <FinanceLogo />
-        <h1 className={styles.title}>Presupuesto</h1>
+        <h1 className={styles.title}>
+          {location === '/config' ? 'Configuración' : 'Presupuesto'}
+        </h1>
       </Link>
       <nav>
-        <Link href='/config' className={styles.link}>
-          <SettingsIcon />
-        </Link>
+        {location !== '/config' && (
+          <Link href='/config' className={styles.link}>
+            <SettingsIcon />
+          </Link>
+        )}
       </nav>
     </header>
   )

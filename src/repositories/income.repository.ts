c9c14556c2a3739076a772e7b52bab0e06d835a7
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-05 21:39:48 -0500

    feature: added repository for income

---
+++ src/repositories/income.repository.ts
@@ -0,0 +1,48 @@
+import { dbService } from '../services/indexedDB.service'
+import type { Income } from '../types/dataBase'
+
+export class IncomeRepository {
+  private readonly storeName = 'income'
+
+  async getAll(): Promise<Income[]> {
+    return dbService.getAll<Income>(this.storeName)
+  }
+
+  async getById(id: number): Promise<Income | undefined> {
+    return dbService.getById<Income>(this.storeName, id)
+  }
+
+  async add(income: Omit<Income, 'id'>): Promise<number> {
+    return dbService.add<Income>(this.storeName, income)
+  }
+
+  async updateIncome(
+    id: number,
+    field: string,
+    update: number | string
+  ): Promise<number> {
+    return dbService.update(this.storeName, id, field, update)
+  }
+
+  async updateDiscount(
+    id: number,
+    field: string,
+    update: number | string
+  ): Promise<number> {
+    return dbService.update(this.storeName, id, field, update)
+  }
+
+  async deleteIncome(id: number): Promise<void> {
+    return dbService.delete(this.storeName, id)
+  }
+
+  async deleteDiscount(id: number): Promise<void> {
+    return dbService.delete(this.storeName, id)
+  }
+
+  async count(): Promise<number> {
+    return dbService.count(this.storeName)
+  }
+}
+
+export const incomeRepository = new IncomeRepository()

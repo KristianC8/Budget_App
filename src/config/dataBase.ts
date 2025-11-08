722fe5f779c5a43aa2a1464506a4cc2914dc97ad
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-08 15:27:36 -0500

    refactor:  format code

--- src/config/dataBase.ts
+++ src/config/dataBase.ts
@@ -1,31 +1,31 @@
-import type { StoreConfig } from '../types/dataBase'
+import type { StoreConfig } from "../types/dataBase";
 
-export const DB_NAME = 'BudgetAppDB'
-export const DB_VERSION = 1
+export const DB_NAME = "BudgetAppDB";
+export const DB_VERSION = 1;
 
 export const STORES: StoreConfig[] = [
   {
-    name: 'expenses',
-    keyPath: 'id',
+    name: "expenses",
+    keyPath: "id",
     autoIncrement: true,
     indexes: [
-      { name: 'createdAt', keyPath: 'createdAt', options: { unique: false } }
-    ]
+      { name: "createdAt", keyPath: "createdAt", options: { unique: false } },
+    ],
   },
   {
-    name: 'categories',
-    keyPath: 'id',
+    name: "categories",
+    keyPath: "id",
     autoIncrement: true,
     indexes: [
-      { name: 'createdAt', keyPath: 'createdAt', options: { unique: false } }
-    ]
+      { name: "createdAt", keyPath: "createdAt", options: { unique: false } },
+    ],
   },
   {
-    name: 'income',
-    keyPath: 'id',
+    name: "income",
+    keyPath: "id",
     autoIncrement: true,
     indexes: [
-      { name: 'createdAt', keyPath: 'createdAt', options: { unique: false } }
-    ]
-  }
-]
+      { name: "createdAt", keyPath: "createdAt", options: { unique: false } },
+    ],
+  },
+];

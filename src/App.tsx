2b69be404ceab299bd60dc73dc3ed1979ec1ec18
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-08 14:20:20 -0500

    feature: added income provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { DBProvider } from './context/DBProvider.tsx'
 import { ExpensesProvider } from './context/ExpensesProvider.tsx'
 import { CategoriesProvider } from './context/CategoriesProvider.tsx'
 import { Router } from './router/index.tsx'
+import { IncomeProvider } from './context/IncomeProvider.tsx'
 
 function App() {
   return (
     <DBProvider>
       <ExpensesProvider>
         <CategoriesProvider>
-          <Router />
+          <IncomeProvider>
+            <Router />
+          </IncomeProvider>
         </CategoriesProvider>
       </ExpensesProvider>
     </DBProvider>

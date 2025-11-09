0ae3f06278104c815747d95e92b0a9fc7de10433
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-09 17:51:00 -0500

    feature: added show income and Modal component

--- src/components/Income.tsx
+++ src/components/Income.tsx
@@ -2,9 +2,17 @@ import { useIncomeDB } from '../hooks/useIncomeDB'
 import { AddIcon } from './icons/AddIcon'
 import { IncomeLogo } from './icons/IncomeLogo'
 import styles from './Income.module.css'
+import { useRef } from 'react'
+import Modal from './Modal'
 
 export const Income = () => {
-  const { income, loading, addIncome } = useIncomeDB()
+  const { income, loading } = useIncomeDB()
+  const modalAddIncomeRef = useRef<HTMLDialogElement>(null)
+  const openModal = () => {
+    if (modalAddIncomeRef.current) {
+      modalAddIncomeRef.current.showModal()
+    }
+  }
   function resetDB() {
     indexedDB.deleteDatabase('BudgetAppDB')
     alert('Base de datos eliminada')
@@ -17,18 +25,21 @@ export const Income = () => {
       </div>
       {loading && <div>Cargando ingresos...</div>}
       {income.length === 0 && <div>Ingresa aquí tus fuentes de ingreso</div>}
-      <button
-        onClick={() => {
-          addIncome({
-            name: 'Inversiones',
-            amount: 220000,
-            discounts: []
-          })
-        }}
-      >
+      <ul>
+        {income &&
+          income.map((income) => (
+            <li key={income.id}>
+              {income.name}: {income.amount}
+              <button>edit</button>
+              <button>delete</button>
+            </li>
+          ))}
+      </ul>
+      <button onClick={openModal}>
         Agregar Ingreso <AddIcon />
       </button>
       <button onClick={resetDB}>Eliminar DB</button>
+      <Modal dialogRef={modalAddIncomeRef} />
     </section>
   )
 }

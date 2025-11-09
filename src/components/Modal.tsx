48d01b5d2784f248774e65d04b0f313b75ff5b25
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-09 17:51:26 -0500

    feature: created modal component to add income

---
+++ src/components/Modal.tsx
@@ -0,0 +1,45 @@
+import styles from './Modal.module.css'
+
+interface modalProps {
+  dialogRef: React.RefObject<HTMLDialogElement | null>
+}
+
+const Modal = ({ dialogRef }: modalProps) => {
+  return (
+    <dialog ref={dialogRef} className={styles.dialog}>
+      <form method='dialog' className={styles.form}>
+        <header>
+          <h2>Agregar fuente de Ingreso</h2>
+          <button aria-label='cerrar' className={styles.close}>
+            ✖
+          </button>
+        </header>
+        <div className={styles.incomeContainer}>
+          <div className={styles.formIncome}>
+            <div className={styles['flex-V']}>
+              <label htmlFor='income'>Cual es tu fuente de Ingreso:</label>
+              <input type='text' name='income' id='income' autoFocus />
+            </div>
+            <div className={styles['flex-V']}>
+              <label htmlFor='amount'>Monto:</label>
+              <div className={styles['flex-H']}>
+                <span className={styles.sign}>$</span>
+                <input type='text' name='amount' id='amount' />
+              </div>
+            </div>
+          </div>
+          <p>Deseas agregar un descuento</p>
+          <div> Agregar descuento</div>
+        </div>
+        <footer>
+          <button type='submit' value='confirm'>
+            Confirmar
+          </button>
+          <button>Cancelar</button>
+        </footer>
+      </form>
+    </dialog>
+  )
+}
+
+export default Modal

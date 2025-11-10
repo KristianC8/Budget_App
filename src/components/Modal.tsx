1eec89523b9ca0e473e736e502ddcbc0c46b2ef8
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-10 18:31:23 -0500

    feature: added inputs for discounts

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,45 +1,166 @@
 import AddIncomeLogo from './icons/AddIncomeLogo'
 import styles from './Modal.module.css'
+import type { Income } from '../types/dataBase'
+import { useForm, useFieldArray, type SubmitHandler } from 'react-hook-form'
+import InfoIcon from './icons/InfoIcon'
+import { helpNumericKeyDown } from '../helpers/helpNumericKeyDown'
 
 interface modalProps {
   dialogRef: React.RefObject<HTMLDialogElement | null>
 }
 
 const Modal = ({ dialogRef }: modalProps) => {
+  console.log('render modal')
+
+  const { register, control, handleSubmit, reset } = useForm<Income>({
+    defaultValues: {
+      name: undefined,
+      amount: undefined,
+      discounts: []
+    }
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'discounts'
+  })
+
+  const onSubmit: SubmitHandler<Income> = (data) => {
+    console.log(data)
+    console.log(data.discounts)
+    reset()
+    dialogRef.current?.close()
+  }
+
+  const addDiscount = () => {
+    append({ name: '', amount: 0 })
+  }
+
+  // const currentDiscounts = watch('discounts')
+
+  const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    dialogRef.current?.close()
+    reset()
+  }
+
   return (
     <dialog ref={dialogRef} className={styles.dialog}>
-      <form method='dialog' className={styles.form}>
-        <header>
-          <div className={styles.title}>
-            <AddIncomeLogo />
-            <h2>Agregar fuente de Ingreso</h2>
-          </div>
-          <button aria-label='cerrar' className={styles.close}>
-            ✖
-          </button>
-        </header>
+      <header className={styles.header}>
+        <div className={styles.title}>
+          <AddIncomeLogo />
+          <h2>Agregar fuente de Ingreso</h2>
+        </div>
+
+        <button
+          aria-label='cerrar'
+          onClick={handleCancel}
+          className={styles.close}
+        >
+          ✖
+        </button>
+      </header>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.incomeContainer}>
           <div className={styles.formIncome}>
             <div className={styles['flex-V']}>
-              <label htmlFor='income'>Cual es tu fuente de Ingreso:</label>
-              <input type='text' name='income' id='income' autoFocus />
+              <label htmlFor='name'>Cual es tu fuente de Ingreso:</label>
+              <input
+                {...register('name', {
+                  required: true,
+                  setValueAs: (value) => value.trim()
+                })}
+                type='text'
+                name='name'
+                id='name'
+                autoComplete='off'
+                maxLength={20}
+                autoFocus
+              />
             </div>
             <div className={styles['flex-V']}>
               <label htmlFor='amount'>Monto:</label>
               <div className={styles['flex-H']}>
                 <span className={styles.sign}>$</span>
-                <input type='text' name='amount' id='amount' />
+                <input
+                  {...register('amount', {
+                    required: true,
+                    min: 1,
+                    max: 99999999999999,
+                    valueAsNumber: true
+                  })}
+                  type='text'
+                  name='amount'
+                  id='amount'
+                  onKeyDown={(e) => {
+                    helpNumericKeyDown(e)
+                  }}
+                  autoComplete='off'
+                  maxLength={14}
+                />
               </div>
             </div>
           </div>
-          <p>Deseas agregar un descuento</p>
-          <div> Agregar descuento</div>
+          <p className={styles.info}>
+            Deseas agregar un descuento
+            <InfoIcon h={'15'} w={'15'} />
+          </p>
+          <div>
+            {fields.length === 0 ? (
+              <div>
+                <p>No hay descuentos agregados</p>
+                <button type='button' onClick={addDiscount}>
+                  Agregar primer descuento
+                </button>
+              </div>
+            ) : (
+              <>
+                {fields.map((field, index) => (
+                  <div key={field.id}>
+                    <div>
+                      <div>
+                        <label>Descuento</label>
+                        <input
+                          {...control.register(`discounts.${index}.name`)}
+                          placeholder='Ej: Salud'
+                        />
+                      </div>
+                      <div>
+                        <label>Monto</label>
+                        <input
+                          {...control.register(`discounts.${index}.amount`)}
+                          type='number'
+                          placeholder='1000'
+                        />
+                      </div>
+                    </div>
+                    <button
+                      type='button'
+                      onClick={() => remove(index)}
+                      title='Eliminar descuento'
+                    >
+                      Borrar
+                    </button>
+                  </div>
+                ))}
+
+                <button type='button' onClick={addDiscount}>
+                  Agregar otro descuento
+                </button>
+              </>
+            )}
+          </div>
         </div>
-        <footer>
-          <button type='submit' value='confirm'>
-            Confirmar
+        <footer className={styles.footer}>
+          <button
+            onClick={handleCancel}
+            className={`${styles.button} ${styles.cancel}`}
+          >
+            Cancelar
           </button>
-          <button>Cancelar</button>
+          <button className={`${styles.button} buttons`} type='submit'>
+            Agregar
+          </button>
         </footer>
       </form>
     </dialog>

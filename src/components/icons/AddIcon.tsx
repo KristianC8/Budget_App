33077f2273bd495dee8e16f03f49650623bb4640
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-09 19:14:35 -0500

    styles: changed color icon to current color

--- src/components/icons/AddIcon.tsx
+++ src/components/icons/AddIcon.tsx
@@ -5,7 +5,6 @@ export const AddIcon = () => {
       width='32'
       height='32'
       viewBox='0 0 24 24'
-      style={{ color: 'var(--primary-color)' }}
     >
       <g fill='currentColor' fillRule='evenodd' clipRule='evenodd'>
         <path d='M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12m10-8a8 8 0 1 0 0 16a8 8 0 0 0 0-16' />

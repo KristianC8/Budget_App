25a14372b57ac287bcecedc71c9cb58120722788
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-15 16:58:48 -0500

    refactor: changed edit icon

--- src/components/icons/EditIcon.tsx
+++ src/components/icons/EditIcon.tsx
@@ -1,16 +1,32 @@
 export const EditIcon = () => {
   return (
+    // <svg
+    //   xmlns='http://www.w3.org/2000/svg'
+    //   width='24'
+    //   height='24'
+    //   viewBox='0 0 24 24'
+    // >
+    //   <path
+    //     fill='currentColor'
+    //     d='M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1q-.15.15-.15.36M20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75z'
+    //   />
+    // </svg>
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
+      width='18'
+      height='18'
       viewBox='0 0 24 24'
-      style={{ color: '#181818' }}
     >
-      <path
-        fill='currentColor'
-        d='M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1q-.15.15-.15.36M20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83l3.75 3.75z'
-      />
+      <g
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
+      >
+        <path d='M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7' />
+        <path d='M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z' />
+      </g>
     </svg>
   )
 }

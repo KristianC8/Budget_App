a5b242d8e13e685f69a67165a916883778d18dc7
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-18 16:19:52 -0500

    refactor: now totals are calculate in context, added quantities formatted

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,23 +1,44 @@
 import { useExpensesDB } from '../hooks/useExpensesDB'
+import { useIncomeDB } from '../hooks/useIncomeDB'
 import styles from './Balance.module.css'
 import { BalanceLogo } from './icons/BalanceLogo'
+import { formatCurrencyParts } from '../utils/Formatter'
 
 export const Balance = () => {
-  const { expenses } = useExpensesDB()
-  const totalExpenses = expenses.reduce((total, currentValue) => {
-    return total + currentValue.amount
-  }, 0)
+  const { totalExpenses } = useExpensesDB()
+  const { netIncome } = useIncomeDB()
+
+  const availableBalance = netIncome - totalExpenses
+
+  const balance = formatCurrencyParts(availableBalance)
+  const income = formatCurrencyParts(netIncome)
+  const expenses = formatCurrencyParts(totalExpenses)
+
   return (
     <section className={`sections ${styles.container}`}>
       <div className={styles.flexHC}>
         <BalanceLogo />
         <h2>Saldo Disponible</h2>
       </div>
-      <div>Avilable Balance</div>
-      <div>Income</div>
-      <div>
-        <span>Expenses:</span>
-        {totalExpenses}
+      <div className={styles.balanceSection}>
+        <span className={styles.sign}>{balance.symbol}</span>
+        <span
+          className={`${availableBalance > netIncome * 0.5 ? styles.high : availableBalance < netIncome * 0.2 ? styles.low : styles.medium}`}
+        >
+          {balance.value}
+        </span>
+      </div>
+      <div className={styles.IncExp}>
+        <div className={styles.incomeSection}>
+          <div className={styles.labels}>Ingresos:</div>
+          <span className={styles.sign}>{income.symbol}</span>
+          <span>{income.value}</span>
+        </div>
+        <div className={styles.expensesSection}>
+          <div className={styles.labels}>Gastos:</div>
+          <span className={styles.sign}>{expenses.symbol}</span>
+          <span>{expenses.value}</span>
+        </div>
       </div>
     </section>
   )

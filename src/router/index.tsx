fe03e1f86f1180121614ef8ee710c1c9d01702c6
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-11-23 14:53:02 -0500

    feature: added route for graph

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'wouter'
 import { Home } from '../pages/Home'
 import { Config } from '../pages/Config'
+import { Graph } from '../pages/Graph'
 
 export const Router = () => (
   <>
     <Switch>
       <Route path='/' component={Home} />
       <Route path='/config' component={Config} />
+      <Route path='/graph' component={Graph} />
       <Route>404: No such page!</Route>
     </Switch>
   </>

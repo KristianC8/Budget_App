f05a58f61715832c383a74570b5f8109bc542596
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-12-12 15:37:09 -0500

    refactor: add new graphs section to graphs page

--- src/pages/Graph.tsx
+++ src/pages/Graph.tsx
@@ -1,14 +1,13 @@
+import { GraphsSection } from '../components/GraphsSection'
 import { Header } from '../components/Header'
 import { useDocumentTitle } from '../hooks/useDocumentTitle'
-import PieChartExpenses from '../components/graphs/PieChartExpenses'
 
 export const Graph = () => {
   useDocumentTitle('Gráfica | Presupuesto App')
   return (
     <>
       <Header />
-      <h2>Gráficos</h2>
-      <PieChartExpenses />
+      <GraphsSection />
     </>
   )
 }
